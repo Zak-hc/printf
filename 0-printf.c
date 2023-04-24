981b55d3cff890d1d3af853357e2af7b56748183
Zack-hc <zhaci001@gmail.com>
2023-04-24 06:37:31 -0700

    printing by us

---
+++ 0-printf.c
@@ -0,0 +1,73 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+*/
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+#include <unistd.h>
+#include <stdarg.h>
+#include "main.h"
+int _printf(const char *format, ...)
+{
+va_list args;
+char percent;
+char *s;
+int c;
+int count = 0;
+va_start(args, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+{
+c = va_arg(args, int);
+write(1, &c, sizeof(char));
+count++;
+break;
+}
+case 's':
+{
+s = va_arg(args, char *);
+while (*s != '\0')
+{
+write(1, s, sizeof(char));
+s++;
+count++;
+}
+break;
+}
+case '%':
+{
+percent = '%';
+write(1, &percent, sizeof(char));
+count++;
+break;
+}
+default:
+{
+percent = '%';
+write(1, &percent, sizeof(char));
+write(1, format, sizeof(char));
+break;
+}
+}
+}
+else
+{
+write(1, format, sizeof(char));
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
+}
