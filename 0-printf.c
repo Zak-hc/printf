e8f62712925a4606f1adf342e56ed96a87cb52df
Zack-hc <zhaci001@gmail.com>
2023-04-24 17:57:05 -0700

    printing by us

--- 0-printf.c
+++ 0-printf.c
@@ -16,9 +16,9 @@ char percent;
 char *to;
 int i;
 int count = 0;
-if (format == NULL || (format[0] == '%' && format + 1 == 0))
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 return (-1);
-if (format[0] == '%' && format[1] == ' ' && (format + 2) == NULL)
+if (format[0] == '%' && format[1] == ' ' && format[2] == '\0')
 return (-1);
 va_start(args, format);
 for (i = 0; format[i] != '\0'; i++)
