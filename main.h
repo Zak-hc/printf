fdf5564e2c34b1769178b4ed1e4fdcbc0f66720e
Zakaria haci <125515540+Zak-hc@users.noreply.github.com>
2023-04-25 13:29:27 +0000

    Update main.h

--- main.h
+++ main.h
@@ -3,7 +3,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <stddef.h>
 int _printf(const char *format, ...);
-int print_str(char *str);
+int print_str(const char *str);
 int print_char(char c);
 #endif
