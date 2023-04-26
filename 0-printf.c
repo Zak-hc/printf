a7a16372c6846515fe0808cf5e958c6462b5194e
Zack-hc <zhaci001@gmail.com>
2023-04-25 17:49:58 -0700

    printing by us

--- 0-printf.c
+++ 0-printf.c
@@ -19,7 +19,12 @@ for (i = 0; format[i] != '\0'; i++)
 if (format[i] == '%')
 {i++;
 switch (format[i])
-{case 'c':
+{case 'd':
+case 'i':
+sprintf(too, "%d", va_arg(args, int));
+count += print_str(too);
+break;
+case 'c':
 count += print_char(va_arg(args, int));
 break;
 case 's':
