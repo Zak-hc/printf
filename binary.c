d14277e5bbc016bbf51b59a6451a5e4648c6b17c
Fad-fad <faduteimi@gmail.com>
2023-04-27 13:35:05 -0700

    printing by us

--- binary.c
+++ binary.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <limits.h>
+#include <math.h>
 int b(va_list args)
 {
 int tab[33];
@@ -12,7 +13,7 @@ sum = 0;
 for (i = 0; i < 32 ; i++)
 {
 if (sum >= div ||
-sum >= INT_MAX - (int)(pow(2, i)) * buffer[i])
+sum >= INT_MAX - (int)(pow(2,i)) * tab[i])
 break;
 tab[i] = (number % 2) ? 1 : 0;
 sum +=  2 ^ i * tab[i];
