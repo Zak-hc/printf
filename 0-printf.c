c387d142936c51f79db68575e7f2c0ae0b11f093
Fad-fad <faduteimi@gmail.com>
2023-04-27 14:22:00 -0700

    printing by us

--- 0-printf.c
+++ 0-printf.c
@@ -7,12 +7,42 @@
 int _printf(const char *format, ...)
 {
 va_list args;
-int count = 0;
+char too[100], *to;
+int i, count = 0;
 if (format == NULL  || (format[0] == '%' && format[1] == '\0')  ||
 (format[0] == '%' && format[1] == ' ' && format[2] == '\0'))
 return (-1);
 va_start(args, format);
-count += _print(format, args);
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] == '%')
+{i++;
+switch (format[i])
+{case 'd': case 'i':
+sprintf(too, "%d", va_arg(args, int));
+count += print_str(too);
+break;
+case 'c':
+count += print_char(va_arg(args, int));
+break;
+case 's':
+to = va_arg(args, char *);
+if (to == NULL)
+count += print_str("(null)");
+else
+count += print_str(to);
+break;
+case '%':
+count += print_char('%');
+break;
+default:
+count += print_char('%');
+count += print_char(format[i]);
+break;
+}}
+else
+count += print_char(format[i]);
+}
 va_end(args);
 return (count);
 }
--- binary.c
+++ binary.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <limits.h>
 #include <stdlib.h>
+/**
+ * b - fonction to entry.
+ * @args: agruments
+ * Return: number of characters
+ */
 int b(va_list args)
 {
 int tab[33];
@@ -8,19 +13,21 @@ int number = va_arg(args, int);
 int i, count = 0;
 int sum;
 int div = number;
-char affich = malloc(sizeof(char) * 33);
+char *affich = malloc(sizeof(char) * 33);
 sum = 0;
 for (i = 0; i < 32 ; i++)
 {
 if (sum >= div ||
-sum >= INT_MAX - (2^i) * tab[i]);
+sum >= INT_MAX - ((2 ^ i) * tab[i]))
+{
 break;
+}
 tab[i] = (number % 2) ? 1 : 0;
-sum +=  2 ^ i * tab[i];
+sum += (2 ^ i) * tab[i];
 number /= 2;
 count++;
-sprintf(affich, "%d", tab[i]);
-print_char(*affich);
+sprintf(&affich[i], "%d", tab[i]);
+print_char(affich[i]);
 }
 free(affich);
 return (count);
