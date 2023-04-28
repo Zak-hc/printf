13bf27d8c331bb4eb28be38f035295ea10805222
Fad-fad <faduteimi@gmail.com>
2023-04-28 18:48:04 +0200

    printing by us

--- 0-printf.c
+++ 0-printf.c
@@ -7,42 +7,13 @@
 int _printf(const char *format, ...)
 {
 va_list args;
-char too[100], *to;
-int i, count = 0;
+int count;
 if (format == NULL  || (format[0] == '%' && format[1] == '\0')  ||
 (format[0] == '%' && format[1] == ' ' && format[2] == '\0'))
 return (-1);
 va_start(args, format);
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] == '%')
-{i++;
-switch (format[i])
-{case 'd': case 'i':
-sprintf(too, "%d", va_arg(args, int));
-count += print_str(too);
-break;
-case 'c':
-count += print_char(va_arg(args, int));
-break;
-case 's':
-to = va_arg(args, char *);
-if (to == NULL)
-count += print_str("(null)");
-else
-count += print_str(to);
-break;
-case '%':
-count += print_char('%');
-break;
-default:
-count += print_char('%');
-count += print_char(format[i]);
-break;
-}}
-else
-count += print_char(format[i]);
-}
+count += _print(format, args);
 va_end(args);
 return (count);
 }
+
